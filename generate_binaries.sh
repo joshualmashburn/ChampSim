440178b943b7fd10755bb040f26acd96b8bd5605
Chrysanthos Pepi <github.flail465@passmail.com>
2024-10-08 18:39:00 -0500

    Update configs

--- generate_binaries.sh
+++ generate_binaries.sh
@@ -1,26 +1,28 @@
-# for f in configs/*.json; do
-#     if [ "$f" == "configs/champsim_config.json" ]; then
-#         continue
-#     fi
-#     echo "Building ${f}"
-#     ./config.sh configs/champsim_config.json ${f} && make
-# done
+for f in configs/*.json; do
+    if [ "$f" == "configs/champsim_config.json" ]; then
+        continue
+    fi
+    echo "Building ${f}"
+    ./config.sh configs/champsim_config.json ${f} && make
+done
 
 # L1I prefetchers:
-# ./config.sh configs/champsim_config.json configs/barsa_config.json && make
-# ./config.sh configs/champsim_config.json configs/bip_config.json && make
-# ./config.sh configs/champsim_config.json configs/djolt_config.json && make
-# # ./config.sh configs/champsim_config.json configs/epi_config.json && make
-# ./config.sh configs/champsim_config.json configs/fnlmma_config.json && make
-# ./config.sh configs/champsim_config.json configs/fnlmma_new_config.json && make
-# # ./config.sh configs/champsim_config.json configs/ISCA_Entangling_4Ke_config.json && make
-# # ./config.sh configs/champsim_config.json configs/ISCA_Entangling_4Ke_opt_bb_split_config.json && make
-# ./config.sh configs/champsim_config.json configs/mana_config.json && make
-# ./config.sh configs/champsim_config.json configs/next_line_config.json && make
-# ./config.sh configs/champsim_config.json configs/no_config.json && make
-# ./config.sh configs/champsim_config.json configs/pips_config.json && make
-# ./config.sh configs/champsim_config.json configs/tap_config.json && make
+# ./config.sh configs/champsim_config.json configs/barsa.json && make
+# ./config.sh configs/champsim_config.json configs/bip.json && make
+# ./config.sh configs/champsim_config.json configs/djolt.json && make
+# # ./config.sh configs/champsim_config.json configs/epi.json && make
+# ./config.sh configs/champsim_config.json configs/fnlmma.json && make
+# ./config.sh configs/champsim_config.json configs/fnlmma_new.json && make
+# # ./config.sh configs/champsim_config.json configs/ISCA_Entangling_4Ke.json && make
+# # ./config.sh configs/champsim_config.json configs/ISCA_Entangling_4Ke_opt_bb_split.json && make
+# ./config.sh configs/champsim_config.json configs/mana.json && make
+# ./config.sh configs/champsim_config.json configs/next_line_instr.json && make
+# ./config.sh configs/champsim_config.json configs/no_instr.json && make
+# ./config.sh configs/champsim_config.json configs/pips.json && make
+# ./config.sh configs/champsim_config.json configs/tap.json && make
 
 # L1D prefetchers:
-# ./config.sh configs/champsim_config.json configs/berti_config.json && make
-./config.sh configs/champsim_config.json configs/ipcp_config.json && make
+# ./config.sh configs/champsim_config.json configs/no.json && make
+# ./config.sh configs/champsim_config.json configs/next_line.json && make
+# ./config.sh configs/champsim_config.json configs/berti.json && make
+# ./config.sh configs/champsim_config.json configs/ipcp.json && make
