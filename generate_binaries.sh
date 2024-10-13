a018a5e7d6aadcd64e247080b9b48499c6fc68a9
Chrysanthos Pepi <github.flail465@passmail.com>
2024-10-13 16:48:18 -0500

    add ip_stride config

--- generate_binaries.sh
+++ generate_binaries.sh
@@ -1,16 +1,16 @@
-for f in configs/*.json; do
-    if [ "$f" == "configs/champsim_config.json" ]; then
-        continue
-    fi
-    echo "Building ${f}"
-    ./config.sh configs/champsim_config.json ${f} && make
-done
+# for f in configs/*.json; do
+#     if [ "$f" == "configs/champsim_config.json" ]; then
+#         continue
+#     fi
+#     echo "Building ${f}"
+#     ./config.sh configs/champsim_config.json ${f} && make
+# done
 
 # L1I prefetchers:
 # ./config.sh configs/champsim_config.json configs/barsa.json && make
 # ./config.sh configs/champsim_config.json configs/bip.json && make
 # ./config.sh configs/champsim_config.json configs/djolt.json && make
-# # ./config.sh configs/champsim_config.json configs/epi.json && make
+# ./config.sh configs/champsim_config.json configs/epi.json && make
 # ./config.sh configs/champsim_config.json configs/fnlmma.json && make
 # ./config.sh configs/champsim_config.json configs/fnlmma_new.json && make
 # # ./config.sh configs/champsim_config.json configs/ISCA_Entangling_4Ke.json && make
@@ -25,4 +25,5 @@ done
 # ./config.sh configs/champsim_config.json configs/no.json && make
 # ./config.sh configs/champsim_config.json configs/next_line.json && make
 # ./config.sh configs/champsim_config.json configs/berti.json && make
+./config.sh configs/champsim_config.json configs/ip_stride.json && make
 # ./config.sh configs/champsim_config.json configs/ipcp.json && make
--- local_champsim.sh
+++ local_champsim.sh
@@ -1,20 +1,20 @@
 mkdir -p cp-data
 mkdir -p wp-data
 
-for trace in $(ls ../new-traces/*.gz); do
-    trace_name=$(basename $trace)
-    trace_name=${trace_name%.gz}
-    echo "Processing ${trace_name}"
+# for trace in $(ls ../new-traces/*.gz); do
+#     trace_name=$(basename $trace)
+#     trace_name=${trace_name%.gz}
+#     echo "Processing ${trace_name}"
 
-    for binary in $(ls bin/*); do
-        binary_name=$(basename $binary)
-        echo "Running ${binary_name}"
+#     for binary in $(ls bin/*); do
+#         binary_name=$(basename $binary)
+#         echo "Running ${binary_name}"
 
-        ${binary} --warmup-instructions 10000 --simulation-instructions 20000 $trace >cp-data/${trace_name}-${binary_name}.txt
-        ${binary} --warmup-instructions 10000 --simulation-instructions 20000 --wrong-path $trace >wp-data/${trace_name}-${binary_name}.txt
-    done
-done
+#         ${binary} --warmup-instructions 10000 --simulation-instructions 20000 $trace >cp-data/${trace_name}-${binary_name}.txt
+#         ${binary} --warmup-instructions 10000 --simulation-instructions 20000 --wrong-path $trace >wp-data/${trace_name}-${binary_name}.txt
+#     done
+# done
 
-# ./bin/champsim-ipcp --warmup-instructions 100000 --simulation-instructions 200000 ../new-traces/tomcat.gz >cp-data/tomcat.txt
-# ./bin/champsim-ipcp --warmup-instructions 100000 --simulation-instructions 200000 --wrong-path ../new-traces/tomcat.gz >wp-data/tomcat.txt
-# gdb -q --tui -ex=r --args ./bin/champsim-djolt --warmup-instructions 100000 --simulation-instructions 200000 --wrong-path ../new-traces/tomcat.gz
+./bin/champsim-l1i-ip_stride --warmup-instructions 10000000 --simulation-instructions 100000000 ../new-traces/tomcat.gz >cp-data/tomcat.txt
+./bin/champsim-l1d-ip_stride --warmup-instructions 1000000 --simulation-instructions 10000000 --wrong-path ../new-traces/tomcat.gz >wp-data/tomcat.txt
+# gdb -q -ex=r --args ./bin/champsim-l1i-epi --warmup-instructions 1000000 --simulation-instructions 10000000 --wrong-path ../new-traces/tomcat.gz
