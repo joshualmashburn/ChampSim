a6e10d59bd36eb66726c74f7481a4ee6d9fbc16c
Chrysanthos Pepi <github.flail465@passmail.com>
2024-10-22 12:33:54 -0500

    Update spp_dev

--- prefetcher/spp_dev_l2c/spp_dev.cc
+++ prefetcher/spp_dev_l2c/spp_dev.cc
@@ -534,8 +534,9 @@ void spp::GLOBAL_REGISTER::update_entry(uint32_t pf_sig, uint32_t pf_confidence,
 
   // Assertion
   if (victim_way >= MAX_GHR_ENTRY) {
-    std::cout << "[GHR] Cannot find a replacement victim!" << std::endl;
-    assert(0);
+    // std::cout << "[GHR] Cannot find a replacement victim!" << std::endl;
+    // assert(0);
+    victim_way = MAX_GHR_ENTRY - 1;
   }
 
   if constexpr (spp::SPP_DEBUG_PRINT) {
